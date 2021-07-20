56878beeea15a50cbf61eecc58bad0adffba45ef
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-07-20 20:30:36 -0300

    Leio arquivo de forma estatica, criando um novo arquivo csv para cada categoria de despesa

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
-puts 'Helo Ruby'
+require 'csv'
+puts 'Processador de Faturas'
 
 file = File.read('./date/nubank-2020-03.csv')
-puts file
+#puts file
+csv = CSV.parse(file,headers: true)
+
+csv.each do |row|
+   expense = row.to_h
+   category_file_path = "./categories/#{expense['category']}.csv"
+   category_file = File.open(category_file_path, "a+")
+   if File.empty?(category_file)
+        category_file.puts csv.headers.join(',')
+   end
+   category_file.write row
+   category_file.close
+end
