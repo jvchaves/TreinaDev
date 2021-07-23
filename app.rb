9c58d57d6bb4448d15e4cef20a63dd1c155dd38d
João Victor Chaves <joao.victor.chaves159@gmail.com>
2021-07-22 21:46:43 -0300

    Movo arquivos ja processados para outra pasta

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'csv'
+require_relative './lib/category_file'
+require_relative './lib/file_processor'
+require 'fileutils'
+
+DATA_DIR = "#{__dir__}/data"
+
+puts 'Processador de Faturas'
+
+Dir.each_child(DATA_DIR).each do |fileName|
+    file = FileProcessor.new("./data/#{fileName}")
+    file.process()
+    FileUtils.mv("./data/#{fileName}","./old/")
+end
+
